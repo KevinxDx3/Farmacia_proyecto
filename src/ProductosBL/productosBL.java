29da82ba2357a0b697500274eef0290406ea1d89
KevinxDx3 <85362446+KevinxDx3@users.noreply.github.com>
2022-09-03 16:22:48 -0500

    CRUD

---
+++ src/ProductosBL/productosBL.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProductosBL;
+
+/**
+ *
+ * @author HOME
+ */
+public class productosBL {
+    
+    int ID;
+    String nombre,Tipo;
+    String Stock,PVP;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+
+    public String getStock() {
+        return Stock;
+    }
+
+    public void setStock(String Stock) {
+        this.Stock = Stock;
+    }
+
+    public String getPVP() {
+        return PVP;
+    }
+
+    public void setPVP(String PVP) {
+        this.PVP = PVP;
+    }
+
+    
+
+   
+    
+    
+    
+    
+    
+}
